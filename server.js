6fed4c0a5305207015b09b9e072a0dd696b878c1
永田健 <nagata-takeshi@nagataken-no-Mac-mini.local>
2016-10-30 00:00:24 +0900

    First Commit

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
-var restify = require('restify');
-var builder = require('botbuilder');
 
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
-});
-
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
-
-bot.dialog('/', function (session) {
-    session.send("はろーわーるどーーー(´・ω・｀)");
-});
-
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
