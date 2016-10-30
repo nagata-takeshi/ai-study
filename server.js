f51c859c22dc1c11ac2d196768df5b8fcae091d6
三越伊勢丹システムソリューションズ <ims03@mitsukoshiisetanshisutemusoryushonzu-no-MacBook-Pro-2.local>
2016-10-30 16:51:43 +0900

    test

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var restify = require('restify');
 var builder = require('botbuilder');
 
@@ -17,13 +18,32 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
+
+var url = ‘https://api.projectoxford.ai/luis/v1/application?id=‘ + process.env.LUIS_ID + 
+          ‘&subscription-key=’ + process.env.LUIS_SUBSCRIPTION_KEY
+var dialog = new builder.LuisDialog(url);
+
+
+
 server.post('/api/messages', connector.listen());
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World update");
+//bot.dialog('/', function (session) {
+//    session.send("Hello World update");
+//});
+
+
+bot.add(‘/‘, dialog);
+
+dialog.on(‘price-forecastiong’, function(session, args){
+   consol.log(‘message:’);
+   consol.log(session.message);
+
+   session.send(‘price-forecastiong!!’);
 });
+
+
 
