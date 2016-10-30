cd11ac66ce23c6473974b899970ddb7ccd1d9039
永田健 <nagata-takeshi@nagataken-no-Mac-mini.local>
2016-10-30 22:35:06 +0900

    First Commit

--- server.js
+++ server.js
@@ -13,21 +13,24 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
   
 // Create chat bot
-//var connector = new builder.ChatConnector({
-//    appId: process.env.MICROSOFT_APP_ID,
-//    appPassword: process.env.MICROSOFT_APP_PASSWORD
-//});
-//var bot = new builder.UniversalBot(connector);
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
 
-var bot = new builder.BotConnectorBot({appId: process.env.APP_ID, 
-appSecret: process.env.APP_SECRET);
-
-var url = 'https://api.projectoxford.ai/luis/v1/application?id=' + process.env.LUIS_ID 
-        + '&subscription-key=' + process.env.LUIS_SUBSCRIPTION_KEY
-
-var dialog = new builder.LuisDialog(url);
+var bot = new builder.UniversalBot(connector);
 
 server.post('/api/messages', connector.listen());
+
+var url = 'https://api.projectoxford.ai/luis/v1/application?id=' + process.env.LUIS_ID
+        + '&subscription-key=' + process.env.LUIS_SUBSCRIPTION_KEY
+
+var recognizer = new builder.LuisRecognizer(url);
+
+var intents = new builder.IntentDialog({
+    recognizers: [recognizer]
+});
+
 
 //=========================================================
 // Bots Dialogs
@@ -37,7 +40,13 @@ server.post('/api/messages', connector.listen());
 //    session.send("Hello World update");
 //});
 
-//bot.add('/', dialog);
+bot.dialog('/', intents);
+
+intents
+     .matches('price-forecastion', function (session, args) {
+        var syouhin = builder.EntityRecognizer.findEntity(args.entities, '商品');
+        session.send(syouhin + "の価格予測ですね");
+     })
 
 //dialog.on('price-forecastiong', function(session, args){
 //   consol.log('message:');
