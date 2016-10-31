2398430d069e4e4a009463afe739b9298afc87df
永田健 <nagata-takeshi@nagataken-no-Mac-mini.local>
2016-11-01 01:16:28 +0900

    First Commit

--- server.js
+++ server.js
@@ -70,8 +70,8 @@ intents
                   json: {'Inputs': {'input1': {'ColumnNames': ['メーカー','燃料タイプ','ドアの数','車体形状','エンジンの場所','エンジンサイズ','値段'],'Values': [['toyota','gas','two','sedan','front','200','0']]}},'GlobalParameters': {}}
               }
 
-//              request(options, function(error, response, body) {
-//              })
+              request(options, function(error, response, body) {
+              })
               session.send(syouhin.entity + "の市場価格ですね");
         } else {
             session.send("test");
