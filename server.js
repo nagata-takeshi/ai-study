a0be4de80457b2ca7b654347f902f0a82d881b0e
三越伊勢丹システムソリューションズ <ims03@mitsukoshiisetanshisutemusoryushonzu-no-MacBook-Pro-2.local>
2016-11-07 14:56:29 +0900

    update

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-//var request = require('superagent');
 var request = require('request');
-
-//request.get("http://httpbin.org/html")
-//       .end(function(res){
-//            console.log(res);
-//       });
+var forecast = require('weather-yahoo-jp').forecast;
 
 //=========================================================
 // Bot Setup
 //=========================================================
-
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
 });
   
 // Create chat bot
@@ -31,9 +25,10 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 // 認識に指定するluis apiのurlを指定
-var url = 'https://api.projectoxford.ai/luis/v1/application?id=' + process.env.LUIS_ID
-        + '&subscription-key=' + process.env.LUIS_SUBSCRIPTION_KEY
-var recognizer = new builder.LuisRecognizer(url);
+//var url = 'https://api.projectoxford.ai/luis/v1/application?id=' + process.env.LUIS_ID
+//        + '&subscription-key=' + process.env.LUIS_SUBSCRIPTION_KEY
+//var recognizer = new builder.LuisRecognizer(url);
+var recognizer = new builder.LuisRecognizer('https://api.projectoxford.ai/luis/v1/application?id=4fcf3965-0fce-44c6-b072-e078b85f95b7&subscription-key=033428336fd540aca26ab6f088d69f09');
 
 // IntentDialogオブジェクトを作成
 var intents = new builder.IntentDialog({
@@ -46,36 +41,98 @@ var intents = new builder.IntentDialog({
 //=========================================================
 
 // 初期ダイアログをintentDialogとして使用する
-bot.dialog('/', intents);
-
-// インテントと処理の結びつけ
-intents
-     .matches('price-forecastiong', function (session, args) {
-        var syouhin = builder.EntityRecognizer.findEntity(args.entities, '商品');
-        if (syouhin) {
-//            request.post("https://ussouthcentral.services.azureml.net/workspaces/ad3872017e32497e8ee877d6011ca157/services/06bfe516cd9247eb828b16cef814664a/execute?api-version=2.0&details=true")
-//                .set('Content-Type', 'application/json')
-//                .set('Authorization', 'Bearer rfpRykhDtQ9hmPFvidOHlXPW9Me61diB+VzXFy1Q9kU5JJv86qZB3sMoz3E35M0PCK4KB0CEzBJfPmNtYhWSFw==')
-//                .set('Content-Length', '47')
-//                .send({"Inputs": {"input1": {"ColumnNames": ["メーカー","燃料タイプ","ドアの数","車体形状","エンジンの場所","エンジンサイズ","値段"],"Values": [[“toyota",“gas",“two",“sedan",“front",“200","0"]]}},"GlobalParameters": {}})
-//                .end(function(res){
-//                   console.log(res);
-//                });
-
-              var options = {
-                  url: 'https://ussouthcentral.services.azureml.net/workspaces/ad3872017e32497e8ee877d6011ca157/services/06bfe516cd9247eb828b16cef814664a/execute?api-version=2.0&details=true',
-                  method: 'POST',
-                  headers: {'Content-Type': 'application/json', 'Authorization': ('Bearer rfpRykhDtQ9hmPFvidOHlXPW9Me61diB+VzXFy1Q9kU5JJv86qZB3sMoz3E35M0PCK4KB0CEzBJfPmNtYhWSFw==')},
-                  json: {'Inputs': {'input1': {'ColumnNames': ["メーカー","燃料タイプ","ドアの数","車体形状","エンジンの場所","エンジンサイズ","値段"],'Values': [["toyota","gas","two","sedan","front","200","0"]]}},'GlobalParameters': {}}
-              }
-              request(options, function(error, response, body) {
-                  session.send(syouhin.entity + "の市場価格(" + body.Results.output1.value.ColumnNames + "):" + body.Results.output1.value.Values[0]);
-              })
-            //  session.send(syouhin.entity + "の市場価格ですね");
-        } else {
-            session.send("test");
-        }
-     })
-
+process.on('uncaughtException',function(err){session.endDialog('aaa');});
+intents.matches('isWeather',
+    function (session, args) {
+        var city = builder.EntityRecognizer.findEntity(args.entities, 'City');
+        var day = builder.EntityRecognizer.findEntity(args.entities, 'Day');
+		var forecastResult = "";
+		var resultText = "";
+		var forecastArea = "";
+        if (city) {
+			forecastArea = city.entity.replace(/\s+/g,"");
+			forecast.get(forecastArea).then(function(forecast) {
+				if (day) {
+					if (day.entity == '明日' || day.entity == 'あした' || day.entity == 'あす') {
+						forecastResult = forecast.tomorrow.text;
+						resultText += '明日の';
+					} else if (day.entity == '今日' || day.entity == '本日' || day.entity == 'きょう') {
+						forecastResult = forecast.today.text;
+						resultText += '今日の';
+					} else {
+						session.endDialog("今日か明日の天気しかわかりませんが何か？万能じゃないんで。");
+					}
+					resultText += forecast.where + "の天気は" + forecastResult + "なんじゃない？あとは気象庁に聞いてくれ。";
+					session.endDialog(resultText);
+				} else {
+					session.endDialog("今日か明日くらい言ってくれてもいいんじゃない？");
+				}
+			}).catch(function(err) {
+				session.endDialog("何故か例外をキャッチしたよ。ぼく死ぬのかな？");
+			});
+		} else {
+			session.endDialog("都道府県ぐらい言ってほしいもんだ。");
+		}
+    }
+)
+.matches('getWeather', [
+    function (session, args) {
+        var city = builder.EntityRecognizer.findEntity(args.entities, 'City');
+        var day = builder.EntityRecognizer.findEntity(args.entities, 'Day');
+		var forecastResult = "";
+		var resultText = "";
+		var forecastArea = "";
+        if (city) {
+			forecastArea = city.entity.replace(/\s+/g,"");
+			forecast.get(forecastArea).then(function(forecast) {
+				if (day) {
+					if (day.entity == '明日' || day.entity == 'あした' || day.entity == 'あす') {
+						forecastResult = forecast.tomorrow.text;
+						resultText += '明日の';
+					} else if (day.entity == '今日' || day.entity == '本日' || day.entity == 'きょう') {
+						forecastResult = forecast.today.text;
+						resultText += '今日の';
+					} else {
+						session.endDialog("今日か明日の天気しかわかりませんが何か？万能じゃないんで。");
+					}
+					resultText += forecast.where + "の天気は" + forecastResult + "だと思うよ。勘だけども。";
+					session.endDialog(resultText);
+				} else {
+					session.endDialog("今日か明日くらい言ってくれてもいいんじゃない？");
+				}
+			}).catch(function(err) {
+				session.endDialog("何故か例外をキャッチしたよ。ぼく死ぬのかな？");
+			});
+		} else {
+			session.endDialog("都道府県ぐらい言ってほしいもんだ。");
+		}
+    }
+])
+.matches('whois', [
+    function (session, args) {
+		session.endDialog("はじめまして僕はbot。名前はまだない。日本の天気に詳しいよ。");
+	}
+])
+.matches('replyHello', [
+    function (session, args) {
+		var greet = builder.EntityRecognizer.findEntity(args.entities, 'Greetings');
+		if (greet) {
+			session.endDialog(greet.entity.replace(/\s+/g,"") + "。");
+		} else {
+			session.endDialog("ん？よ、よお。（。。。よくわからないけど挨拶っぽい）");
+		}
+	}
+])
+.matches('replyGoodBye', [
+    function (session, args) {
+		var greet = builder.EntityRecognizer.findEntity(args.entities, 'Greetings');
+		if (greet) {
+			session.endDialog(greet.entity.replace(/\s+/g,"") + "。天気が知りたくなったらまた呼んでロボ。");
+		} else {
+			session.endDialog("別れの挨拶を言われている気がする。ではまたね。");
+		}
+	}
+])
+.onDefault(function (session) { session.endDialog("日本語でok") });
 
 
