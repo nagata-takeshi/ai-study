5e753c647fce3c44cf8009bfe62cc685ecb3ae0b
三越伊勢丹システムソリューションズ <ims03@mitsukoshiisetanshisutemusoryushonzu-no-MacBook-Pro-2.local>
2016-11-07 14:37:10 +0900

    update

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-var forecast = require('weather-yahoo-jp').forecast;
 var recognizer = new builder.LuisRecognizer('https://api.projectoxford.ai/luis/v1/application?id=4fcf3965-0fce-44c6-b072-e078b85f95b7&subscription-key=033428336fd540aca26ab6f088d69f09');
 var intents = new builder.IntentDialog({ recognizers: [recognizer] });
 var bot = new builder.UniversalBot(connector);
